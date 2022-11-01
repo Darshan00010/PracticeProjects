f0c461a0cebb88027f36d7cd0ad823f9e87bfba6
Darshan00010 <109279124+Darshan00010@users.noreply.github.com>
2022-11-01 02:11:14 -0700

    I built calculator in second way(Grid method)

---
+++ Calculator/app.js
@@ -0,0 +1,19 @@
+
+let buttons=document.querySelectorAll("button");
+let string=""
+
+buttons.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+            if(e.target.innerHTML=="="){
+                string=eval(string);
+                document.querySelector('input').value=string;
+            }else if(e.target.innerHTML=="DEL"){
+                string="";
+                document.querySelector('input').value=string;
+            }else{
+                string=string+e.target.innerHTML
+                document.querySelector('input').value=string;
+            }
+            
+    })
+})
